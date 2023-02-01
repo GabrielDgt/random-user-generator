e25db9405bec45246e4e78688b0ad5733e5c2df0
GabrielDgt <gaboz.03@gmail.com>
2023-02-01 20:07:50 +0100

    added fetch to get data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import { useState, useEffect } from 'react'
+
+const API_URL = 'https://random-data-api.com/api/v2/users?response_type=json'
+
 export function App () {
+  const [data, setData] = useState(null)
+  const [user, setUser] = useState()
+  const [avatar, setAvatar] = useState()
+  const [firstName, setFirstName] = useState()
+  const [lastName, setLastName] = useState()
+  const [email, setEmail] = useState()
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(resp => resp.json())
+      .then(data => {
+        setData(data)
+      })
+    // window.addEventListener('load', handleCLick)
+  }, [data])
+
+  const handleCLick = () => {
+    const { username, avatar, first_name, last_name, email } = data
+    setUser(username)
+    setAvatar(avatar)
+    setFirstName(first_name)
+    setLastName(last_name)
+    setEmail(email)
+  }
+
   return (
-    <h1>Test</h1>
+    <main className='container'>
+      <section className='cardContainer'>
+        <span className='imgContainer'><img src={avatar} alt='User avatar' /></span>
+        <span>
+          <h3>{firstName} {lastName}</h3>
+          <h4 className='details'>Username: <span>{user}</span></h4>
+          <h6>Email: {email}</h6>
+        </span>
+      </section>
+      <button className='generatorBtn' onClick={handleCLick}>Generate Random User</button>
+    </main>
   )
 }
